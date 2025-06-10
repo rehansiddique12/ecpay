cfc8748f83781afaf13a420362287ab24b360895
SubtainMustafa <subtainmustafa9@gmail.com>
2025-06-10 17:09:16 +0500

    Add Permission Last Hour

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -942,8 +942,10 @@ $languages = [
                                 <div data-i18n="Deposit Log">{{ __('sidebar.deposit_log') }}</div>
                             </a>
                         </li>
+                        @endif
+                        @if (adminAccessRoute(config('role.deposit_last_hour_report.access.view')))
                         <li
-                            class="menu-item {{ in_array(Route::currentRouteName(), ['admin.payment.log', 'admin.payment.search']) ? 'active' : '' }}">
+                            class="menu-item {{ in_array(Route::currentRouteName(), ['admin.payment.log2', 'admin.payment.search']) ? 'active' : '' }}">
                             <a href="{{ route('admin.payment.log2') }}" class="menu-link">
                                 <i class="menu-icon icon-base ti tabler-file-dollar"></i>
                                 <div data-i18n="Deposit log2">{{ __('sidebar.deposit_log') }} (Last Hour)</div>
