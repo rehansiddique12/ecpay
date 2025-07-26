5bf6e5d4a9a4a0901fa0fc8fb98c0ec3b60164a8
IftikharAliMughal <iftikhar.mughal.a@gmail.com>
2025-07-26 15:53:22 +0500

    update

---
+++ app/Http/Middleware/AdminAuthorizeMiddleware copy.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use App\Models\TwoStepVerification;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+
+class AdminAuthorizeMiddleware
+{
+    public function handle(Request $request, Closure $next)
+    {
+        ////
+
+
+        $user = Auth::guard('admin')->user();
+        $list = collect(config('role'))->pluck(['access'])->flatten();
+        $filtered = $list->intersect($user->admin_access);
+
+
+        $loginTimestamp = Session::get('login_timestamp');
+        if($user->last_session_id!=$loginTimestamp){
+            Auth::guard('admin')->logout();
+            Session::flush();
+            return redirect()->route('admin.login')->with('error', 'You have been logged out because your account was accessed from another location.');
+        }
+
+        // if ($user->id == 1) {
+        //     return $next($request);
+        // }
+
+        if(!in_array($request->route()->getName(), $list->toArray()) ||  in_array($request->route()->getName(), $filtered->toArray()) ){
+
+            $TwoStepVerification = TwoStepVerification::where('user_id', $user->id)->where('type', 'Admin')
+                ->first();
+            if($TwoStepVerification){
+                if($TwoStepVerification->g_auth_status=="Yes"){
+                    return $next($request);
+                }
+            }
+            if($request->path()=="admin/twoFA" || $request->path()=="admin/logout"){
+               return $next($request);
+            }else{
+               
+               return  redirect()->route('admin.twoFA');
+            }
+
+
+            // return $next($request);
+        }
+
+        if($request->route()->getName()=="admin.dashboard"){
+            return  redirect()->route('admin.profile'); 
+         }
+
+        return  redirect()->route('admin.403');
+    }
+}
