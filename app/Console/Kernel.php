509969e1a21d3b6894357f639ab7b47a7a04a949
IftikharAliMughal <iftikhar.mughal.a@gmail.com>
2025-09-01 12:51:41 +0500

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -18,6 +18,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')->hourly();
         $schedule->command('app:send_callback')->everyMinute();
         $schedule->command('app:daily-creat-summaries')->dailyAt('00:01');
+        $schedule->command('command:assign-account-to-payout')->everyMinute();
     }
 
     /**
